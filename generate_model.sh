29ed4d8e1091ce9d18ef0c463b32314a6e72dd2e
Andrew Morgan <jp19060@bc4login2.bc4.acrc.priv>
2023-02-16 14:49:26 +0000

    ingnore all results for github

--- generate_model.sh
+++ generate_model.sh
@@ -5,7 +5,8 @@
 
 #Run PIFu
 cd pifuhd
-python -m apps.simple_test
+#-r is resolution so that can be changed 
+python -m apps.simple_test -r 256 --use_rect -i ./sample_images/
 # python apps/clean_mesh.py -f ./results/pifuhd_final/recon
 python -m apps.render_turntable -f results/pifuhd_final/recon -ww 512 -hh 512
 
