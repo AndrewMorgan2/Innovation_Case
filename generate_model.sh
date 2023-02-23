b28f4456bfb3f5487fb13629eec3d7c9755c26ff
Andrew Morgan <jp19060@bc4login1.bc4.acrc.priv>
2023-02-23 12:30:40 +0000

    Ignore logs

--- generate_model.sh
+++ generate_model.sh
@@ -17,7 +17,7 @@ cd ..
 #Run PIFu
 cd pifuhd
 #-r is resolution so that can be changed 
-python -m apps.simple_test -r 1024 --use_rect -i ./sample_images/
+python -m apps.simple_test -r 768 --use_rect -i ./sample_images/
 python apps/clean_mesh.py -f ./results/pifuhd_final/recon
 python -m apps.render_turntable -f results/pifuhd_final/recon -ww 512 -hh 512
 
